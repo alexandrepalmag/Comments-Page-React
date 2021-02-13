9a49deeaa076bfa73b74051020277201f6b692fd
Alexandre Palma Gonçalves <alepalmag85ti@gmail.com>
2021-02-13 07:08:01 -0300

    Working with properties.

---
+++ comments-page/src/components/Comment.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Comment.css';
+
+//JSX
+const Comment = props => (
+    <div className="Comment">
+
+        <h2>{props.name}</h2>
+        <p>{props.email}</p>
+        <p>{props.children}</p>
+        <p>{props.dateNow.toString()}</p>
+
+    </div>
+);
+
+export default Comment;
