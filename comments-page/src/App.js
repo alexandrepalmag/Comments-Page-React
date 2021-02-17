64e5a154bf8b0ce573dc4a6e18ec45918a5f7fc8
Alexandre Palma Gonçalves <alepalmag85ti@gmail.com>
2021-02-17 08:02:05 -0300

    Function to fill the fields.

--- comments-page/src/App.js
+++ comments-page/src/App.js
@@ -23,7 +23,48 @@ class App extends Component {
         message: 'Hi, I am fine?'
       }
 
-    ]
+    ],
+
+    newComm: {
+      name: '',
+      email: '',
+      message: ''
+    }
+
+  }
+
+  addComment = (event) => {
+
+    event.preventDefault();
+
+    const newComment = {
+
+      name: 'Jhon',
+      email: 'john@gmail.com',
+      dateNow: new Date(2020, 3, 19),
+      message: 'Hi, how are you?'
+
+    }
+
+    /* let listComm = this.state.commentsList;
+    listComm.push(newComment);
+
+    this.setState({ commentsList: listComm });
+    or
+    */
+
+    this.setState({
+
+      commentsList: [...this.state.commentsList, newComment]
+
+    })
+
+  }
+
+  typeInField = event => {
+
+    const {name, value} = event.target;
+    this.setState({ newComm: { ...this.state.newComm, [name]: value } });
 
   }
 
@@ -46,6 +87,35 @@ class App extends Component {
           )
           )
         }
+
+        <form method="post" onSubmit={this.addComment}>
+          <h2>Add New Comment</h2>
+          <div>
+            <input
+              type="text"
+              name="yourName"
+              value={this.state.newComm.name}
+              onChange={this.typeInField}
+              placeholder="Type Your Name" />
+          </div>
+          <div>
+            <input
+              type="email"
+              name="yourEmail"
+              value={this.state.newComm.email}
+              onChange={this.typeInField}
+              placeholder="Type Your e-mail" />
+          </div>
+          <div>
+            <textarea
+              name="message"
+              value={this.state.newComm.message}
+              onChange={this.typeInField}
+              rows="4" />
+          </div>
+          <button type="submit">Add Comment</button>
+        </form>
+
       </div>
 
     );
